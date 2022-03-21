e5493233c81f79e8272e53b86209af3f94463228
johnsonza <55718475+johnsonza@users.noreply.github.com>
2022-03-21 17:19:20 -0500

    Created basic Navbar
    Includes a logo, menu, and sign in button. Can be refactored to align buttons to the corners.

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Nav, NavLink, Bars, NavMenu, NavBtn, NavBtnLink} from './NavbarElements'
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavLink to="/">
+                    <h1>Logo</h1>
+                </NavLink>
+                <Bars />
+                <NavMenu>
+                    <NavLink to="/about" activestyle='true'>
+                        About
+                    </NavLink>
+                </NavMenu>
+                <NavBtn>
+                    <NavBtnLink to="/signin">Sign In</NavBtnLink>
+                </NavBtn>
+            </Nav>
+        </>
+    );
+};
+
+export default Navbar
